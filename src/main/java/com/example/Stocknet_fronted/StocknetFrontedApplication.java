3f0a7b724ef28b6e92e493cfa61312625ceb817d
BryamCode <bryamfernandezuriarte22@gmail.com>
2026-02-06 17:41:52 -0500

    Creacion del fronted de la web

---
+++ src/main/java/com/example/Stocknet_fronted/StocknetFrontedApplication.java
@@ -0,0 +1,13 @@
+package com.example.Stocknet_fronted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocknetFrontedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocknetFrontedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Stocknet_fronted/StocknetFrontedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Stocknet_fronted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocknetFrontedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
